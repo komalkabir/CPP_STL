327fd763c5199621b3b261830edb5f8dae3089af
Komal Kabir <komalkabir9@gmail.com>
2021-04-18 18:23:25 +0530

    Added swap function

--- pair.cpp
+++ pair.cpp
@@ -1,4 +1,3 @@
-// pair code example
 #include<iostream>
 #include<utility>
 using namespace std;
@@ -11,22 +10,33 @@ int main()
  pair<int,int> number;
  number.first= 100;
  number.second=200;
-
  cout<<number.first<<" ";
  cout<<number.second<<endl;
  
  // pair <data_type1, data_type2> pair_name(value1, value2);
  //with initialization
 
- pair<int,string> name(1,"Abc");
+ pair<string,string> name("Xyz","Abc");
+
  cout<<name.first<<" ";
  cout<<name.second<<endl;
 
- //make_pair function
+ //make_pair()  :initialize using function
 
  pair<string,string> full_name;
+
  full_name=make_pair("John","carter");
  cout<<full_name.first<<" ";
- cout<<full_name.second;
+ cout<<full_name.second<<endl;
+
+ // swap() : swaps content of one pair with content of another pair
+ cout<<"Before swap:"<<endl;
+ cout<<"Pair 1:"<<name.first<<" "<<name.second<<endl;
+ cout<<"Pair 2:"<<full_name.first<<" "<<full_name.second<<endl;
+ 
+ cout<<"After swap:"<<endl;
+ full_name.swap(name);
+  cout<<"Pair 1:"<<name.first<<" "<<name.second<<endl;
+ cout<<"Pair 2:"<<full_name.first<<" "<<full_name.second<<endl;
 
  }
