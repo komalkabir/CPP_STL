578afa98ccb2afabdbf9b2307562e6a430c523db
Komal Kabir <komalkabir9@gmail.com>
2021-04-18 17:04:13 +0530

    Added initializtion types and make_pair function

--- pair.cpp
+++ pair.cpp
@@ -1,2 +1,32 @@
 // pair code example
+#include<iostream>
+#include<utility>
+using namespace std;
 
+int main()
+{ 
+   // pair <data_type1, data_type2> pair_name;
+   // without initialization
+
+ pair<int,int> number;
+ number.first= 100;
+ number.second=200;
+
+ cout<<number.first<<" ";
+ cout<<number.second<<endl;
+ 
+ // pair <data_type1, data_type2> pair_name(value1, value2);
+ //with initialization
+
+ pair<int,string> name(1,"Abc");
+ cout<<name.first<<" ";
+ cout<<name.second<<endl;
+
+ //make_pair function
+
+ pair<string,string> full_name;
+ full_name=make_pair("John","carter");
+ cout<<full_name.first<<" ";
+ cout<<full_name.second;
+
+ }
