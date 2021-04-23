e3ed644e75ff8f27584cc4e3b4ff4d9495120ede
Komal Kabir <komalkabir9@gmail.com>
2021-04-23 18:39:50 +0530

    added vector

--- vector.cpp
+++ vector.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {   
-    //creating vector
+    //creating vector ,accessed and traveresed using iterators
     vector<int> num;
     for (int i = 0; i < 6; i++)
         num.push_back(i);            
@@ -47,7 +47,50 @@ int main()
         cout<<*i<<" ";
 
     id.clear();                       // clear() removes all the elements
-    cout<<"\n"<<"All elements are removed as size is: "<<id.size();  
+    cout<<"\n"<<"All elements are removed as size is: "<<id.size()<<"\n";  
 
+    vector<int> roll;
+
+    roll.assign(5,20);     //fill the array with value=20, five times
+
+    cout<<"\n"<<"Roll values: ";
+    for (auto i = roll.begin(); i!= roll.end(); i++)   
+        cout<<*i<<" ";
+    
+    roll.push_back(30);              //push_back() inserts elements from back
+    roll.push_back(40);
+    cout<<"\n"<<"After push_back(): ";
+     for (auto i = roll.begin(); i!= roll.end(); i++)   
+        cout<<*i<<" ";
+
+    roll.pop_back();                 //pop_back() removes element from back
+    cout<<"\n"<<"After pop_back(): ";
+    for (auto i = roll.begin(); i!= roll.end(); i++)   //Last element is removed
+        cout<<*i<<" ";
+    
+
+    roll.insert(roll.begin()+2,45);            //inserts value before the given position
+    cout<<"\n"<<"Value is inserted at 2nd index: ";
+      for (auto i = roll.begin(); i!= roll.end(); i++)   
+        cout<<*i<<" ";
+
+    vector<int> num1,num2;
+    num1.assign(4,30);
+    num2.assign(4,10);
+
+    num1.swap(num2);                            //swapping values of num1 with num2;
+     for (auto i = num1.begin(), j=num2.begin(); i!= num1.end(); i++,j++)   
+        cout<<"\n"<<"num1:"<<*i<<" "<<"num2:"<<*j<<" ";
+
+    
+    //emplace() and emplace_back()
+
+    num1.emplace(num1.begin()+3,100);           //inserts value at given position
+    num2.emplace_back(200);                     //inserts value at end of vector
+       
+    cout<<"\n"<<"num1 after using emplace():"<<num1.at(2)<<" "<<num1.at(3)<<" "<<num1.at(4);
+    cout<<"\n"<<"num2 last element:"<<num2.back();
+
+    
     return 0;
 }
