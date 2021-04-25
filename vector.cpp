cf7b85c79041905c97a3f2d3e4641ded3e831faa
Komal Kabir <komalkabir9@gmail.com>
2021-04-25 19:19:20 +0530

    Added vector

--- vector.cpp
+++ vector.cpp
@@ -9,12 +9,16 @@ int main()
     for (int i = 0; i < 6; i++)
         num.push_back(i);            
     
-    // begin() and end() returns iterator pointing to first and last element respectively
+    // begin() and end() returns iterator pointing to 
+    //first and last element respectively
+
     cout<<"First to last elements:"<<endl;
     for (auto i = num.begin(); i!= num.end(); i++)     //auto identified i to be iterator
         cout<<*i<<" ";
     
-    // rbegin() and rend() returns reverse iterator pointing to last and first element respectively
+    // rbegin() and rend() returns reverse iterator
+    // pointing to last and first element respectively
+
       cout<<endl<<"Last to first elements:"<<endl;
     for (auto i= num.rbegin() ; i!= num.rend() ; i++)    //moves from last to first
         cout<<*i<<" ";
@@ -30,7 +34,7 @@ int main()
     cout<<"\nVector resized to: "<<name.size()<<"\n";      // vector is resized from 5 to 3
 
     vector<int> id;
-    if(id.empty()==true)                                //checks if vector is empty
+    if(id.empty()==true)              //checks if vector is empty
      cout<<"Vector is empty"<<endl;                     
     else
      cout<<"Vector is not empty";
@@ -40,13 +44,13 @@ int main()
     vector<int>::iterator ite;
     id={1,2,3,4,5,6};
 
-    ite=id.begin();                   // erase() used to remove element at particular index
-    id.erase(++ite);                  //erases element at 1st position
+    ite=id.begin();           // erase() used to remove element at particular index
+    id.erase(++ite);          //erases element at 1st position
 
     for (auto i = id.begin(); i!= id.end(); i++)   
         cout<<*i<<" ";
 
-    id.clear();                       // clear() removes all the elements
+    id.clear();      // clear() removes all the elements
     cout<<"\nAll elements are removed as size is: "<<id.size()<<"\n";  
 
     vector<int> roll;
@@ -57,7 +61,7 @@ int main()
     for (auto i = roll.begin(); i!= roll.end(); i++)   
         cout<<*i<<" ";
     
-    roll.push_back(30);              //push_back() inserts elements from back
+    roll.push_back(30);      //push_back() inserts elements from back
     roll.push_back(40);
     cout<<"\nAfter push_back(): ";
      for (auto i = roll.begin(); i!= roll.end(); i++)   
@@ -71,15 +75,15 @@ int main()
 
     roll.insert(roll.begin()+2,45);            //inserts value before the given position
     cout<<"\nValue is inserted at 2nd index: ";
-      for (auto i = roll.begin(); i!= roll.end(); i++)   
-        cout<<*i<<" ";
+    for (auto i = roll.begin(); i!= roll.end(); i++)   
+     cout<<*i<<" ";
 
     vector<int> num1,num2;
     num1.assign(4,30);
     num2.assign(4,10);
 
     num1.swap(num2);                            //swapping values of num1 with num2;
-     for (auto i = num1.begin(), j=num2.begin(); i!= num1.end(); i++,j++)   
+    for (auto i = num1.begin(), j=num2.begin(); i!= num1.end(); i++,j++)   
         cout<<"\nnum1:"<<*i<<" "<<"num2:"<<*j<<" ";
 
     
@@ -89,7 +93,7 @@ int main()
     num2.emplace_back(200);                     //inserts value at end of vector
 
        cout<<"\nnum1 values: ";
-   for (auto i = num1.begin(); i!= num1.end(); i++)     
+    for (auto i = num1.begin(); i!= num1.end(); i++)     
         cout<<*i<<" ";
 
        cout<<"\nnum2 values: ";
